536d1d58853882288cf0ccb8ffaf747d6ab6874a
amitk506 <amitkumar50608@gmail.com>
2022-06-30 18:14:13 +0530

    null safety added

--- lib/timeline_item.dart
+++ lib/timeline_item.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 
 class TimelineItem {
   final String title;
-  final String subtitle;
-  final String description;
+  final String? subtitle;
+  final String? description;
   final Widget child;
   final Color bubbleColor;
 
   const TimelineItem({
-    @required this.title,
+    /*required*/ required this.title,
     this.subtitle,
     this.description,
-    @required this.child,
-    @required this.bubbleColor,
+    /*required*/ required this.child,
+    /*required*/ required this.bubbleColor,
   });
 }
